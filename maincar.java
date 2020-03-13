5d5e82097b7e70c38ff6ce01b6bf64fa3b02096c
ROSHAN1112 <roshan28880@gmail.com>
2020-03-12 20:44:39 -0700

    roshan first commit

---
+++ maincar.java
@@ -0,0 +1,20 @@
+public class maincar {
+
+    public static void main(String[] args) {
+
+        car c=new car();
+        powercar constructor=new powercar();
+
+
+        c.run();
+        c.light();
+        constructor.run();
+        constructor.light();
+        constructor.power();
+
+
+
+
+    }
+
+}
